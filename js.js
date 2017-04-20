74ec4af1ae690ee6a32249c7f31fe81cd85e0635
Bikram Bhattacharya <you@example.com>
2017-04-21 00:55:16 +0530

    added index

---
+++ js.js
@@ -0,0 +1,41 @@
+$( function()
+{    $('#audio').hide(0);
+    $("h1").css("color","red").fadeOut(5000);    
+	$(document).keydown(function(e)
+    {
+       
+        switch(e.which){
+       
+                            case 37:  $('.box').animate({
+                                         left: "-=20px",
+                                         fontSize: "+=2px"
+                                      }, function() {
+                                   
+                                     });
+                                     break;
+                            case 38: $('.box').animate({
+                                        top: "-=20px",
+                                        opacity: "+=0.1"
+                                     },function(){
+
+                                     });
+                                     break;
+                            case 39:$('.box').animate({
+                                         left: "+=20px",
+                                         fontSize: "-=2px"
+                                     },function(){
+
+                                     });
+                                    break;
+                            case 40: $('.box').animate({
+                                        top: "+=20px",
+                                        opacity: "-=0.1"
+                                     },function(){
+
+                                     });
+                                    break;
+                        }  
+    } );
+	
+
+});
